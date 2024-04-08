a8b45e6c7fbeca78fbcd2481ee76fb9f6bf65034
Mjames951 <aspc.james2@gmail.com>
2024-04-07 22:13:31 -0500

    moved some stuff around

--- PowerPlant/src/components/SignUp.vue
+++ PowerPlant/src/components/SignUp.vue
@@ -1,3 +1,41 @@
 <template>
     This is the signup page 
+    <form>
+        email:
+        <input type="text" id="email">
+        password:
+        <input type="text" id="password">
+    </form>
+    <button @click="attemptLogin">Login</button>
 </template>
+
+<script setup lang="ts">
+import axios from 'axios';
+import { ref } from 'vue';
+
+let UserID;
+
+function attemptLogin() {
+    const Login = () => {
+        let form = new FormData();
+        let email = (<HTMLInputElement>document.getElementById("email")).value;
+        let password = (<HTMLInputElement>document.getElementById("password")).value;
+        form.append("email", email);
+        form.append("password", password);
+        axios.post('', form)
+            .then(response => 
+            {
+                UserID = response.data;
+                sessionStorage.setItem("UserID", UserID)
+            },
+            (error) =>
+            {
+                alert(error.message);
+            });
+    };
+    Login();
+};
+
+
+</script>
+
