421741696f07d49b89e53c378a031ef518364f84
Mjames951 <aspc.james2@gmail.com>
2024-04-08 00:15:26 -0500

    signing off

--- PowerPlant/src/components/UserProfile.vue
+++ PowerPlant/src/components/UserProfile.vue
@@ -1,9 +1,24 @@
 <template>
     This is the userprofile page 
+    <p>{{ UserData }}</p>
+    <p>edit user data:</p><br>
+    <form>
+        first name:
+        <input type="text" id="first_name">
+        last name:
+        <input type="text" id="last_name">
+        email:
+        <input type="text" id="email">
+        password:
+        <input type="text" id="password">
+    </form>
+    <button @click="UpdateUserData">Edit User</button>
 </template>
 <script setup lang="ts">
 import axios from 'axios';
 import { ref } from 'vue';
+
+let UserData = ref([]);
 
 function GetUserData() {
     const Getit = () => {
@@ -13,8 +28,7 @@ function GetUserData() {
         axios.post('http://127.0.0.1:8000/getuserprofile/', form)
             .then(response => 
             {
-                UserID = response.data;
-                sessionStorage.setItem("UserID", UserID)
+                UserData.value.push(response.data);
             },
             (error) =>
             {
