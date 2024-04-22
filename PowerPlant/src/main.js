7cbf6e839ae6f1c8b2141a7e21894a1e2610dc2d
Ryan Acuna <racuna@unomaha.edu>
2024-04-22 17:03:49 -0500

    added axios and removed css max width for page

--- PowerPlant/src/main.js
+++ PowerPlant/src/main.js
@@ -3,9 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import { Axios } from 'axios'
 
 const app = createApp(App)
 
-app.use(router)
+app.use(router, Axios)
 
 app.mount('#app')
